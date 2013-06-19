ef4d63301c3b444835de81870e4cd6107f7efed1
404pnf <404pnf@exmaple.org>
2013-06-19 17:37:15 +0800

    1. use CONSTANT_V for names of IO object; 2. shorten scripts by eliminating duplication; 3. snesible variable names

--- segments-to-novels.rb
+++ segments-to-novels.rb
@@ -3,15 +3,17 @@ require 'pp'
 #require 'pstore'
 require 'erubis'
 
-input = ARGV[0]
+INPUT = './db/book_segments.csv'
+OUTPUT = './output-html'
+TPL_FILE = './book.tpl.erubis.html'
 
 # we need this gsub
-# or we get `block (2 levels) in shift': Missing or stray quote in line XXXX (CSV::MalformedCSVError)
+# or else: we get `block (2 levels) in shift': Missing or stray quote in line XXXX (CSV::MalformedCSVError)
 # ref: http://stackoverflow.com/questions/14534522/ruby-csv-parsing-string-with-escaped-quotes
+# if original csv is
 # 173,"Yukihiro \"The Ruby Guy\" Matsumoto","Japan"
 # The \" is typical Unix whereas Ruby CSV expects ""
-
-c = CSV.parse(File.read(input).gsub('\"', '""'))
+c = CSV.parse(File.read(INPUT).gsub('\"', '""'))
 
 zh_en_bookid_tuple = c.map do |r|
   _, _, zh, en, _, _, id, _ = r
@@ -24,21 +26,21 @@ zh_en_bookid_tuple.each do |tuple|
   zh, en, id = tuple
   # NB: id is string, therefore
   # id is "42" but not 42
-  book_hash[id] << zh 
-  book_hash[id] << en
+  # 每本书对应的值是数组，按顺序一段中文一段英文
+  book_hash[id] << [zh, en] 
 end
-pp book_hash["188"] # keys are STRING representation of number
+#pp book_hash["178"][2][1] # keys are STRING representation of number
 
 # generate html of individual book
-book_hash.each do |bookid, content|
+book_hash.each do |bookid, paragraphs|
   title = bookid
-  paragraphs = content.each_slice(2).map {|e| e}
   # here comes erubis
-  eruby = Erubis::Eruby.new(File.read('book.tpl.erubis.html'))       # create Eruby object
+  eruby = Erubis::Eruby.new(File.read(TPL_FILE)) # create Eruby object
   html_str =  eruby.result(binding())   # TODO get result; all local variables are available in the template, might not be a good idea  
   # same old file writing
-  p "generating #{title}.html"
-  File.open("#{title}.html", "w") do |file|
+  #p "generating #{OUTPUT}/#{title}.html"
+  p "go checkout #{OUTPUT}"
+  File.open("#{OUTPUT}/#{title}.html", "w") do |file|
     file.puts html_str
   end
 end
