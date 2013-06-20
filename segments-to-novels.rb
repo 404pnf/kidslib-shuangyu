3eff1dbc3dae04db19127cc39bf97516b1beae3e
404pnf <404pnf@exmaple.org>
2013-06-20 13:52:20 +0800

    1. use more CONSANT_V  2.  method to copy assets(css, js) to output folder

--- segments-to-novels.rb
+++ segments-to-novels.rb
@@ -2,10 +2,14 @@ require 'csv'
 require 'pp'
 #require 'pstore'
 require 'erubis'
+require 'fileutils'
+require 'kramdown'
 
 INPUT = './db/book_segments.csv'
 OUTPUT = './output-html'
-TPL_FILE = './book.tpl.erubis.html'
+VIEW_FOLDER = 'views'
+TPL_FN = 'book.erubis.html'
+TPL_FILE =  "#{VIEW_FOLDER}/#{TPL_FN}"
 
 # we need this gsub
 # or else: we get `block (2 levels) in shift': Missing or stray quote in line XXXX (CSV::MalformedCSVError)
@@ -23,10 +27,10 @@ end
 
 book_hash = Hash.new{ |hash,key| hash[key] = Array.new }
 zh_en_bookid_tuple.each do |tuple|
-  zh, en, id = tuple
   # NB: id is string, therefore
   # id is "42" but not 42
   # 每本书对应的值是数组，按顺序一段中文一段英文
+  zh, en, id = tuple
   book_hash[id] << [zh, en] 
 end
 #pp book_hash["178"][2][1] # keys are STRING representation of number
@@ -38,12 +42,18 @@ book_hash.each do |bookid, paragraphs|
   eruby = Erubis::Eruby.new(File.read(TPL_FILE)) # create Eruby object
   html_str =  eruby.result(binding())   # TODO get result; all local variables are available in the template, might not be a good idea  
   # same old file writing
-  #p "generating #{OUTPUT}/#{title}.html"
-  p "go checkout #{OUTPUT}"
+  p "generating #{OUTPUT}/#{title}.html"
+  #p "go checkout #{OUTPUT}"
   File.open("#{OUTPUT}/#{title}.html", "w") do |file|
     file.puts html_str
   end
 end
+
+# copy necessary js/css files from views/ to output-html
+# ref: http://www.ruby-doc.org/stdlib-2.0/libdoc/fileutils/rdoc/FileUtils.html#method-c-copy
+FileUtils.cp_r Dir.glob("#{VIEW_FOLDER}/*.js"), OUTPUT, :noop => true, :verbose => true
+FileUtils.cp_r Dir.glob("#{VIEW_FOLDER}/*.css"), OUTPUT, :noop => true, :verbose => true
+
 
 
 
