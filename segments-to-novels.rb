33ff448521020cbec5c5434c87cb98e674aee05e
404pnf <404pnf@exmaple.org>
2013-06-26 11:26:00 +0800

    清理脚本

--- segments-to-novels.rb
+++ segments-to-novels.rb
@@ -6,6 +6,7 @@ require 'fileutils'
 require 'kramdown'
 
 INPUT = './db/book_segments.csv'
+BOOK_TITLES = './db/book_titles.csv'
 OUTPUT = './output-html'
 VIEW_FOLDER = 'views'
 TPL_FN = 'book.erubis.html'
@@ -35,17 +36,26 @@ zh_en_bookid_tuple.each do |tuple|
 end
 #pp book_hash["178"][2][1] # keys are STRING representation of number
 
+# get book titles
+book_title_arr = CSV.parse(File.read(BOOK_TITLES).gsub('\"', '""'))
+book_title_hash = {}
+book_title_arr.each do |(id, zh_title, en_title)|
+  book_title_hash[id] = [zh_title, en_title]
+end
+
 # generate html of individual book
 book_hash.each do |bookid, paragraphs|
-  title = bookid
+  id = bookid
+  title_arr = book_title_hash["#{bookid}"]
+  title = title_arr.join("<br>") unless title_arr.nil?
+  p title
   # here comes erubis
   eruby = Erubis::Eruby.new(File.read(TPL_FILE)) # create Eruby object
   html_str =  eruby.result(binding())   # TODO get result; all local variables are available in the template, might not be a good idea  
   # same old file writing
-  p "generating #{OUTPUT}/#{title}.html"
   p "generating #{OUTPUT}/#{id}.html"
   #p "go checkout #{OUTPUT}"
-  File.open("#{OUTPUT}/#{title}.html", "w") do |file|
+  File.open("#{OUTPUT}/#{id}.html", "w") do |file|
     file.puts html_str
   end
 end
@@ -54,17 +64,3 @@ end
 # ref: http://www.ruby-doc.org/stdlib-2.0/libdoc/fileutils/rdoc/FileUtils.html#method-c-copy
 FileUtils.cp_r Dir.glob("#{VIEW_FOLDER}/*.js"), OUTPUT, :noop => true, :verbose => true
 FileUtils.cp_r Dir.glob("#{VIEW_FOLDER}/*.css"), OUTPUT, :noop => true, :verbose => true
-
-
-
-
-  
-
-
-
-
-
-
-
-  
-  
