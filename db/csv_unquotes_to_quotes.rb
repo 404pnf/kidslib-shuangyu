25eea659d4e00088179ab6a7c622522b2efad9a1
404pnf <404pnf@exmaple.org>
2013-09-05 15:32:02 +0800

    因为双语的bookid字段被lg设置为整数了，因此不能接受xin001这种，因此我花费了很长时间重新更新表中id并同步到文件名中

--- db/csv_unquotes_to_quotes.rb
+++ db/csv_unquotes_to_quotes.rb
@@ -1,15 +1,14 @@
-
+require 'csv'
 
 def unquotes_to_quotes(file)
-	CSV.read(file).map { |e| e.to_csv(force_quotes: true)}
+	#CSV.read(file).map { |e| e.to_csv(force_quotes: true)}
+	CSV.read(file).map { |e| e.to_csv(force_quotes: true).sub("\n", '') }.join("\n")
 end
 
-def main
-	Dir['*.csv'].each do |file|
-		content = unquotes_to_quotes(file)
-		new_filename = filename + '.quotes.csv'
-		File.write(new_filename, content)
-	end
+def main(file)
+	content = unquotes_to_quotes(file)
+	new_file = file + '.quotes.csv'
+	File.write(new_file, content)
 end
 
-main if __FILE__ == $PROGRAME_NAME
+main(ARGV[0]) if __FILE__ == $PROGRAM_NAME
