c7564fcf1e747b06577690b5821138041519050c
404pnf <404pnf@exmaple.org>
2013-09-05 09:58:38 +0800

    简单整理的脚本，重新生成图书

--- 05-xin-csv-to-html.rb
+++ 05-xin-csv-to-html.rb
@@ -11,34 +11,46 @@ TPL_FILE = 'views/book.erubis.html'
 BOOK_TITLES = 'db/xin-book-titles.csv'
 SUFFIX = '.csv'
 
-files = Find.find(CSV_FILEFODLER).select { |f| f =~ /csv$/}
-titles = CSV.readlines(BOOK_TITLES).each_with_object(Hash.new) do |e, a|
-  id, *title = e
-  a[id] = title
-end
-# pp titles
-files.each do |file|
-  id = File.basename(file, '.csv')
-  title = titles[id].join(' | ') unless titles[id].nil?
-  paragraphs = CSV.readlines file
+def xin_csv_2_html
 
-  eruby = Erubis::Eruby.new(File.read(TPL_FILE)) # create Eruby object
-  html_str =  eruby.result(binding())   # TODO get result; all local variables are available in the template, might not be a good idea
+  files = Find.find(CSV_FILEFODLER).select { |f| f =~ /#{ SUFFIX }$/}
+  titles = CSV.readlines(BOOK_TITLES).each_with_object(Hash.new) do |e, a|
+    id, *title = e
+    a[id] = title
+  end
+  # pp titles
+  files.each do |file|
+    id = File.basename(file, SUFFIX)
+    title = titles[id].join(' | ') unless titles[id].nil?
+    paragraphs = CSV.readlines file
 
-  p "generating #{OUTPUT}/#{id}.html: #{title}"
-  File.write("#{OUTPUT}/#{id}.html", html_str)
+    eruby = Erubis::Eruby.new(File.read(TPL_FILE)) # create Eruby object
+    html_str =  eruby.result(binding())   # TODO get result; all local variables are available in the template, might not be a good idea
+
+    p "generating #{OUTPUT}/#{id}.html: #{title}"
+    File.write("#{OUTPUT}/#{id}.html", html_str)
   end
 end
 
-# copy necessary js/css files from views/ to output-html
-# ref: http://www.ruby-doc.org/stdlib-2.0/libdoc/fileutils/rdoc/FileUtils.html#method-c-copy
-FileUtils.cp_r Dir.glob("#{VIEW_FOLDER}/*.js"), OUTPUT, :noop => false, :verbose => true
-FileUtils.cp_r Dir.glob("#{VIEW_FOLDER}/*.css"), OUTPUT, :noop => false, :verbose => true
+def copy_js_css_2_ouputfolder
+  # copy necessary js/css files from views/ to output-html
+  # ref: http://www.ruby-doc.org/stdlib-2.0/libdoc/fileutils/rdoc/FileUtils.html#method-c-copy
+  FileUtils.cp_r Dir.glob("#{VIEW_FOLDER}/*.js"), OUTPUT, :noop => false, :verbose => true
+  FileUtils.cp_r Dir.glob("#{VIEW_FOLDER}/*.css"), OUTPUT, :noop => false, :verbose => true
+end
 
-# 友情提示
-p ''
-p '------------'
-p '友情提示'
-p '-------------'
-p ''
-p "生成的文件在： #{OUTPUT}"
+def suceed_msg
+  # 友情提示
+  p ''
+  p '------------'
+  p '友情提示'
+  p '-------------'
+  p ''
+  p "生成的文件在： #{OUTPUT}"
+end
+
+if __FILE__ == $PROGRAM_NAME
+  xin_csv_2_html
+  #copy_js_css_2_ouputfolder
+  #suceed_msg
+end
